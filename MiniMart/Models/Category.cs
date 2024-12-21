d3eca52afd4e23974140a43535c36513e7354f41
Ana-drea <huiwen_zhou@hotmail.com>
2024-12-20 18:03:10 -0800

    frontend for add product, view products by category

--- MiniMart/Models/Category.cs
+++ MiniMart/Models/Category.cs
@@ -1,10 +1,13 @@
-﻿namespace MiniMart.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MiniMart.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
+        [JsonIgnore] // 避免循环引用
         public ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
